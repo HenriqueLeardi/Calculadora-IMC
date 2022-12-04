c856918e755624ab1810b656c2b84fad034dc7ca
HenriqueLeardi <hojegamepvp@gmail.com>
2022-12-04 11:30:16 -0300

    Adicionando arquivos

---
+++ script.js
@@ -0,0 +1,34 @@
+function calculoImc (event) {
+    event.preventDefault();
+
+    let resultado;
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+
+    let imc = peso / (altura * altura);
+
+    if (imc < 17) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está muito abaixo do peso!"
+    }else if (imc > 17 && imc <= 18.49) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está muito abaixo do peso!"
+    }else if (imc >= 18.5 && imc < 24.99) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está no peso ideal!"
+    }else if (imc > 25 &&imc <= 29.9) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está levemente acima do peso!"
+    }else if (imc >= 30 && imc <= 34.9) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está em obesidade grau 1!"
+    }else if (imc >= 35 && imc <= 39.9) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está mem obesidade grau 2 (severa)"
+    }else if (imc >= 40) {
+        resultado = document.getElementById("resultado");
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está em obesidade grau 3 (mórbida)"
+    }else if (imc === null) {
+        alert("Coloque um valor válido!")
+    };
+}
