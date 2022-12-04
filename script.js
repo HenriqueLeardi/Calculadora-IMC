f0c60067c3279d4dccf8e02472eb5d0bba948fc8
HenriqueLeardi <hojegamepvp@gmail.com>
2022-12-04 11:55:48 -0300

    correções básicas

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function calculoImc (event) {
         resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está muito abaixo do peso!"
     }else if (imc > 17 && imc <= 18.49) {
         resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está muito abaixo do peso!"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está abaixo do peso!"
     }else if (imc >= 18.5 && imc < 24.99) {
         resultado = document.getElementById("resultado");
         resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está no peso ideal!"
@@ -24,11 +24,9 @@ function calculoImc (event) {
         resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está em obesidade grau 1!"
     }else if (imc >= 35 && imc <= 39.9) {
         resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está mem obesidade grau 2 (severa)"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está em obesidade grau 2 (severa)"
     }else if (imc >= 40) {
         resultado = document.getElementById("resultado");
         resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está em obesidade grau 3 (mórbida)"
-    }else if (imc === null) {
-        alert("Coloque um valor válido!")
     };
 }
