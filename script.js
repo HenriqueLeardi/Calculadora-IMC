9d8d949590190f1eece4758d3d67e780a4df751b
HenriqueLeardi <hojegamepvp@gmail.com>
2022-12-06 22:25:15 -0300

    Otimizando algumas coisas

--- script.js
+++ script.js
@@ -7,26 +7,24 @@ function calculoImc (event) {
 
     let imc = peso / (altura * altura);
 
+    resultado = document.getElementById("resultado");
+
     if (imc < 17) {
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está muito abaixo do peso!"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc.toFixed(2) + "<br> Cuidado você está muito abaixo do peso!"
     }else if (imc > 17 && imc <= 18.49) {
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está abaixo do peso!"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc.toFixed(2) + "<br> Você está abaixo do peso!"
     }else if (imc >= 18.5 && imc < 24.99) {
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está no peso ideal!"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc.toFixed(2) + "<br> Você está no peso ideal!"
     }else if (imc > 25 &&imc <= 29.9) {
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está levemente acima do peso!"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc.toFixed(2) + "<br> Cuidado você está levemente acima do peso!"
     }else if (imc >= 30 && imc <= 34.9) {
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está em obesidade grau 1!"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc.toFixed(2) + "<br> Cuidado você está em obesidade grau 1!"
     }else if (imc >= 35 && imc <= 39.9) {
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Cuidado você está em obesidade grau 2 (severa)"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc.toFixed(2) + "<br> Cuidado você está em obesidade grau 2 (severa)"
     }else if (imc >= 40) {
-        resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<br> Seu resultado foi: " + imc + "<br> Você está em obesidade grau 3 (mórbida)"
+        resultado.innerHTML = "<br> Seu resultado foi: " + imc.toFixed(2) + "<br> Você está em obesidade grau 3 (mórbida)"
     };
+
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
 }
